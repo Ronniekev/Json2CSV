4925d872b12599b7958bbfb4f963744812acfdcb
Kevin Penate <156158153+Ronniekev@users.noreply.github.com>
2025-08-08 15:45:21 -0700

    json/server
    Logic to create a csv file from json data. server for zeroMQ

---
+++ json_csv.py
@@ -0,0 +1,25 @@
+import csv
+import os
+
+def create_csv(plan_name, plan, plan_time, allocated_time):
+    """ Function creates a CSV file containing a list of tasks for the day.
+    Params: - plan list with {id} and {duration}
+    - plan name
+    -total plan time
+    -total allocated time"""
+
+    file_name = f'{plan_name}.csv'
+    download = os.path.join(os.path.expanduser('~'), 'Downloads')
+    path = os.path.join(download, file_name)
+
+    with open(path, 'w', newline='', encoding='utf-8') as csvfile:
+        writer =csv.writer(csvfile, delimiter='  ')
+        writer.writerow(plan_name)
+        writer.writerow('    '+ plan[1].keys())
+        for task in plan:
+            row = ['[ ]'] + list(task.values())
+            writer.writerow(row)
+        writer.writerow([f"This plan uses {plan_time} minutes of {allocated_time} minutes available.\n"])
+        return f'File {file_name} created, can be found in downloads folder.'
+
+
---
+++ server.py
@@ -0,0 +1,33 @@
+import zmq
+import json
+from json_csv import create_csv
+
+environment = zmq.Context()
+replySocket = environment.socket(zmq.REP)
+replySocket.bind("tcp://*:5564") #confirm its available 
+print("Listening for messages ...")
+#wait for message from program
+
+while True:
+    message = replySocket.recv_string()
+    print("Request recieved")
+
+
+    data = json.loads(message)
+    
+    plan = data['plan']
+    plan_time = data['plan_time']
+    plan_name = data['plan_name']
+    allocated_time = data['allocated_time']
+
+    if not plan or not plan_time or not plan_name or not allocated_time:
+        response_message = {"Error": 'Missing variables'}
+
+    else:
+        response_data = create_csv(plan_name, plan, plan_time, allocated_time)
+        response_message = {"Success": response_data}
+
+        
+        
+    
+    replySocket.send_string(json.dumps(response_message))
